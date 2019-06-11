50ab81611ab7e2d12969c60820ce8df59a0c6c19
Kenneth Reitz <me@kennethreitz.org>
2019-06-11 14:01:28 +0200

    fix tests, maybe

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -10,9 +10,11 @@ def write_config(filename, config):
         f.write(config)
 
 
-@pytest.mark.parametrize("data", [
-    {
-        "config": """{
+@pytest.mark.parametrize(
+    "data",
+    [
+        {
+            "config": """{
             "auths": {
                 "https://index.docker.io/v1/": {
                     "auth": "b64_username_password"
@@ -20,36 +22,39 @@ def write_config(filename, config):
             }
         }
         """,
-        "valid": True
-    },
-    {
-        "config": "not json",
-        "valid": False
-    }
-])
-def test_containerconfig(cli, data):
+            "valid": True,
+        },
+        {"config": "not json", "valid": False},
+    ],
+)
+def test_containers(cli, data):
     _, config_filename = tempfile.mkstemp()
 
     config_name = uuid.uuid4().hex
 
     write_config(config_filename, data['config'])
 
-    create_cmd = cli('containerconfig', 'create', config_name, config_filename)
-    list_cmd = cli('containerconfig', 'list')
+    create_cmd = cli('containers', 'create', config_name, config_filename)
+    list_cmd = cli('containers', 'list')
 
     if data['valid']:
         assert create_cmd.return_code == 0
         assert config_name in list_cmd.out
+
         data['config'].replace('b64_username_password', 'new_b64_password')
         write_config(config_filename, data['config'])
-        update_cmd = cli('containerconfig', 'update',
-                         config_name, config_filename)
+        update_cmd = cli(
+            'containerconfig', 'update', config_name, config_filename
+        )
+
         assert update_cmd.return_code == 0
         assert 'Updated' in update_cmd.out
-        get_cmd = cli('containerconfig', 'get', config_name)
+
+        get_cmd = cli('containers', 'get', config_name)
         assert get_cmd.return_code == 0
         assert json.loads(data['config']) == json.loads(get_cmd.out)
-        delete_cmd = cli('containerconfig', 'delete', config_name)
+
+        delete_cmd = cli('containers', 'delete', config_name)
         assert delete_cmd.return_code == 0
         assert 'Deleted' in delete_cmd.out
     else:
